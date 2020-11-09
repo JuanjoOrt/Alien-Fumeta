cf1edd6dfd4c4b3aaebe512408f6f55b4528f481
juanjose.ortiz <juanjose.ortiz@optimissa.com>
2020-11-09 16:33:17 +0100

    key new added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 const w2gLogic = async (message, text) => {
   const response = await fetch('https://w2g.tv/rooms/create.json', {method: 'POST', 
   body: { 
-    "w2g_api_key" : "spucwwgugq7aqjjy4nyja1w37epmnzsp69qwvxxjwbeq3m6ep2q17ym3tbz527jr",
+    "w2g_api_key" : "af39dcef5e497058504202919cf134d081ed6cc74f7f30c1b2df38deff22f746",
     "share" : text,  // URL of the video to share - optional
 }});
   const json = await response.json();
