121b8aebfe3f768283b4fd146064c3e6846da306
juanjose.ortiz <juanjose.ortiz@optimissa.com>
2020-11-11 23:01:05 +0100

    added elvui

--- constants/singleQuerys.js
+++ constants/singleQuerys.js
@@ -9,6 +9,7 @@ module.exports = {
             {command: '!moto', response: 'https://www.twitch.tv/orslok/clip/JoyousPowerfulSpindlePanicBasket?filter=clips&range=all&sort=time'},
             {command: '!mujeres', response: 'https://www.twitch.tv/orslok/clip/KindArborealThymeKappaClaus?filter=clips&range=7d&sort=time'},
             {command: '!antonio', response: 'Maricón'},
+            {command: '!elvui', response: 'https://wago.io/OWfs94A2I'},
             {command: '!poppeer', response: 'https://www.warcraftlogs.com/character/eu/sanguino/poppeer'},
             {command: '!viernes', response: 'https://twitter.com/NilGarcia10/status/1271337186676350977'},
             {command: '!tense', response: 'https://media1.tenor.com/images/0d234e9b7067d6076b16663b3a2daa3c/tenor.gif?itemid=15600543'},
