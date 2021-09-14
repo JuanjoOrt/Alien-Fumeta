b30a5c7195f8eafd48957041be284b8c20e74915
Juanjo Ortiz <juanjoseortizgames@gmail.com>
2021-09-14 23:50:38 +0200

    removed antonio

--- constants/singleQuerys.js
+++ constants/singleQuerys.js
@@ -5,8 +5,7 @@ module.exports = {
         return [
             {command: '!alien', response: 'Fumeta 👽 '},
             {command: '!brakal', response: 'https://cdn.discordapp.com/attachments/316381660725903360/754462121667854426/brakalSupermeneo.gif'},
-            {command: '!bokeron', response: 'https://www.youtube.com/watch?v=Ae_2A4pCtj8'},
-            {command: '!antonio', response: 'Maricón'},
+            {command: '!bokeron', response: 'https://www.youtube.com/watch?v=Ae_2A4pCtj8'}
         ]
     } 
     
